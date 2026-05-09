cedc7e1e86cb415a2bdac1ee3363c73e7f0a47ae
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-09 11:08:51 -0400

    refactor: create UrlInjector and move URL-injection-related logic to it

--- src-tauri/src/commands/content.rs
+++ src-tauri/src/commands/content.rs
@@ -32,6 +32,66 @@ impl Resource {
     }
 }
 
+struct UrlInjector<'a> {
+    current_file_path: &'a str,
+}
+
+impl<'a> UrlInjector<'a> {
+    fn new(current_file_path: &'a str) -> Self {
+        UrlInjector { current_file_path }
+    }
+
+    fn inject_resource_urls(&self, content: String) -> String {
+        // E.g. an <img src="images/image.png"> tag inside /OEBPS/Text/chapter.xhtml will be transformed into <img src="epub://localhost/OEBPS/images/image.png">
+        SRC_REGEX
+            .replace_all(&content, |caps: &regex::Captures| {
+                let file_path = String::from(&caps[1]);
+                let final_src = if UrlInjector::is_local_resource(&file_path) {
+                    format!("epub://localhost{}", self.normalize_file_path(file_path))
+                } else {
+                    file_path
+                };
+
+                format!(r#"src="{final_src}""#)
+            })
+            .into_owned()
+    }
+
+    fn normalize_file_path(&self, path: String) -> String {
+        // In case an EPUB passes a percent-encoded path
+        let decoded = percent_decode_str(&path).decode_utf8_lossy();
+
+        let current_file_path = Path::new(self.current_file_path);
+        let path = Path::new(decoded.as_ref());
+
+        let mut normalized_path = if !path.starts_with("/") {
+            let current_dir = current_file_path.parent().unwrap_or_else(|| Path::new("/"));
+            PathBuf::new().join(current_dir)
+        } else {
+            PathBuf::new()
+        };
+
+        for component in path {
+            if component == ".." {
+                normalized_path.pop();
+            } else {
+                normalized_path = normalized_path.join(component);
+            }
+        }
+
+        let err_msg = "File paths MUST be encoded using UTF-8: https://www.w3.org/TR/epub-33/#sec-zip-container-zipreqs";
+        utf8_percent_encode(normalized_path.to_str().expect(err_msg), PATH_CHARS).to_string()
+    }
+
+    fn is_local_resource(src: &str) -> bool {
+        !(src.starts_with("http://")
+            || src.starts_with("https://")
+            || src.starts_with("data:")
+            || src.starts_with("epub://")
+            || src.starts_with('#'))
+    }
+}
+
 #[tauri::command]
 pub fn get_epub_content(state: State<'_, Arc<AppData>>) -> Result<String, String> {
     let source = &state.source;
@@ -50,69 +110,17 @@ pub fn get_epub_content(state: State<'_, Arc<AppData>>) -> Result<String, String
         // entry
         if let Some(resource) = epub.manifest().by_id(id) {
             let href = resource.href().as_str();
+            let injector = UrlInjector::new(href);
+
             let cur_content = epub
                 .read_resource_str(resource)
                 .map_err(|e| e.to_string())?;
 
-            content += &inject_resource_urls(cur_content, href);
+            content += &injector.inject_resource_urls(cur_content);
         }
     }
 
     Ok(content)
-}
-
-fn inject_resource_urls(content: String, current_file_path: &str) -> String {
-    // E.g. an <img src="images/image.png"> tag inside /OEBPS/Text/chapter.xhtml will be transformed into <img src="epub://localhost/OEBPS/images/image.png">
-    SRC_REGEX
-        .replace_all(&content, |caps: &regex::Captures| {
-            let file_path = String::from(&caps[1]);
-            let final_src = if is_local_resource(&file_path) {
-                format!(
-                    "epub://localhost{}",
-                    normalize_file_path(file_path, current_file_path)
-                )
-            } else {
-                file_path
-            };
-
-            format!(r#"src="{final_src}""#)
-        })
-        .into_owned()
-}
-
-fn is_local_resource(src: &str) -> bool {
-    !(src.starts_with("http://")
-        || src.starts_with("https://")
-        || src.starts_with("data:")
-        || src.starts_with("epub://")
-        || src.starts_with('#'))
-}
-
-// To derive file paths, this should be useful: https://www.w3.org/TR/epub-33/#sec-container-iri
-fn normalize_file_path(path: String, current_file_path: &str) -> String {
-    // In case an EPUB passes a percent-encoded path
-    let decoded = percent_decode_str(&path).decode_utf8_lossy();
-
-    let current_file_path = Path::new(current_file_path);
-    let path = Path::new(decoded.as_ref());
-
-    let mut normalized_path = if !path.starts_with("/") {
-        let current_dir = current_file_path.parent().unwrap_or_else(|| Path::new("/"));
-        PathBuf::new().join(current_dir)
-    } else {
-        PathBuf::new()
-    };
-
-    for component in path {
-        if component == ".." {
-            normalized_path.pop();
-        } else {
-            normalized_path = normalized_path.join(component);
-        }
-    }
-
-    let err_msg = "File paths MUST be encoded using UTF-8: https://www.w3.org/TR/epub-33/#sec-zip-container-zipreqs";
-    utf8_percent_encode(normalized_path.to_str().expect(err_msg), PATH_CHARS).to_string()
 }
 
 pub(crate) fn get_resource(epub_source: &PathBuf, path: &str) -> Result<Resource, Box<dyn Error>> {
@@ -149,7 +157,10 @@ mod tests {
             let path = String::from("images/image.png");
             let current_file_path = "/OEBPS/cover.xhtml";
             let expected_path = "/OEBPS/images/image.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -158,7 +169,10 @@ mod tests {
             let path = String::from("../images/image.png");
             let current_file_path = "/OEBPS/Text/chapter1.xhtml";
             let expected_path = "/OEBPS/images/image.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -167,7 +181,10 @@ mod tests {
             let path = String::from("/OEBPS/images/image.png");
             let current_file_path = "/OEBPS/Text/chapter1.xhtml";
             let expected_path = "/OEBPS/images/image.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -176,7 +193,10 @@ mod tests {
             let path = String::from("image.png");
             let current_file_path = "/OEBPS/image.png";
             let expected_path = "/OEBPS/image.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -184,7 +204,10 @@ mod tests {
             let path = String::from("../../images/image.png");
             let current_file_path = "/OEBPS/Text/Section/chapter1.xhtml";
             let expected_path = "/OEBPS/images/image.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -193,7 +216,10 @@ mod tests {
             let path = String::from("../../../../escape.png");
             let current_file_path = "/cover.xhtml";
             let expected_path = "/escape.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -202,7 +228,10 @@ mod tests {
             let path = String::from("images/image.png");
             let current_file_path = "/cover.xhtml";
             let expected_path = "/images/image.png";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -211,8 +240,10 @@ mod tests {
             let current_file_path = "/OEBPS/cover.xhtml";
             let expected_path = "/OEBPS/images/my%20image.png";
 
+            let injector = UrlInjector::new(current_file_path);
+
             // Essentially testing that already-encoded
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
 
         #[test]
@@ -221,13 +252,18 @@ mod tests {
             let path = String::from("../../fonts/arial.ttf");
             let current_file_path = "/OEBPS/Text/chapter1.xhtml";
             let expected_path = "/fonts/arial.ttf";
-            assert_eq!(expected_path, normalize_file_path(path, current_file_path));
+
+            let injector = UrlInjector::new(current_file_path);
+
+            assert_eq!(expected_path, injector.normalize_file_path(path));
         }
     }
 
     #[test]
     fn test_inject_urls() {
         let current_file_path = "/OEBPS/cover.xhtml";
+
+        let injector = UrlInjector::new(current_file_path);
 
         // Taken from No Longer Human:
         let original_xhtml = String::from(
@@ -273,7 +309,7 @@ mod tests {
             </html>
         "#;
 
-        let modified_xhtml = inject_resource_urls(original_xhtml, current_file_path);
+        let modified_xhtml = injector.inject_resource_urls(original_xhtml);
 
         eprintln!("{modified_xhtml}");
         assert_eq!(modified_xhtml, expected_xhtml);
